f3f9e409385b9caf61434f182b00d48771968997
Ebisiemen Ajayi <ebisiemen@gmail.com>
2019-12-23 19:33:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,43 @@
-class Shoe 
-  attr_accessor :brand, :color, :size, :material, :condition
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Shoe 
+#   attr_accessor :brand, :color, :size, :material, :condition
   
-  def initialize(brand)
-    @brand = brand 
-  end   
+#   def initialize(brand)
+#     @brand = brand 
+#   end   
   
-  def cobble 
-    puts "Your shoe is as good as new!"
-    @condition = "new"
-  end   
-end   
+#   def cobble 
+#     puts "Your shoe is as good as new!"
+#     @condition = "new"
+#   end   
+# end   
